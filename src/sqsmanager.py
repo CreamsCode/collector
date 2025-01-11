447d78ca67942ccbaa6f176fa7bdc9e4be0ebcc0
Carlos <carlos.mendoza103@alu.ulpgc.es>
2025-01-11 16:49:34 +0000

    Testing compressing.

--- src/sqsmanager.py
+++ src/sqsmanager.py
@@ -1,19 +1,28 @@
 import boto3
 import json
+import gzip
+import base64
 
 class SQSManager:
     def __init__(self, queue_url, aws_access_key_id, aws_secret_access_key, region_name="us-east-1"):
         self.queue_url = queue_url
         self.client = boto3.client(
             'sqs',
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,
             region_name=region_name
         )
 
     def send_message(self, message_body):
-        response = self.client.send_message(
-            QueueUrl=self.queue_url,
-            MessageBody=json.dumps(message_body)
-        )
-        return response
+        def send_message(self, message_body):
+            # Convertir el mensaje a JSON y comprimirlo
+            message_json = json.dumps(message_body).encode('utf-8')
+            compressed_message = base64.b64encode(gzip.compress(message_json)).decode('utf-8')
+
+            print(f"Compressed message size: {len(compressed_message)} bytes")
+            if len(compressed_message) > 262144:
+                raise ValueError("Compressed message size exceeds 256 KB limit.")
+
+            response = self.client.send_message(
+                QueueUrl=self.queue_url,
+                MessageBody=compressed_message
+            )
+            return response
