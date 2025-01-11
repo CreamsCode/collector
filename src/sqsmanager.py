470b1b0d8f6f45b7431a4f4a2c86467c840bea7d
Carlos <carlos.mendoza103@alu.ulpgc.es>
2025-01-11 16:55:26 +0000

    Testing compressing.

--- src/sqsmanager.py
+++ src/sqsmanager.py
@@ -12,17 +12,16 @@ class SQSManager:
         )
 
     def send_message(self, message_body):
-        def send_message(self, message_body):
-            # Convertir el mensaje a JSON y comprimirlo
-            message_json = json.dumps(message_body).encode('utf-8')
-            compressed_message = base64.b64encode(gzip.compress(message_json)).decode('utf-8')
+        # Convertir el mensaje a JSON y comprimirlo
+        message_json = json.dumps(message_body).encode('utf-8')
+        compressed_message = base64.b64encode(gzip.compress(message_json)).decode('utf-8')
 
-            print(f"Compressed message size: {len(compressed_message)} bytes")
-            if len(compressed_message) > 262144:
-                raise ValueError("Compressed message size exceeds 256 KB limit.")
+        print(f"Compressed message size: {len(compressed_message)} bytes")
+        if len(compressed_message) > 262144:
+            raise ValueError("Compressed message size exceeds 256 KB limit.")
 
-            response = self.client.send_message(
-                QueueUrl=self.queue_url,
-                MessageBody=compressed_message
-            )
-            return response
+        response = self.client.send_message(
+            QueueUrl=self.queue_url,
+            MessageBody=compressed_message
+        )
+        return response
