a9ccbaf23b4dde580819cf0dd354c429e345eb85
Carlos <carlos.mendoza103@alu.ulpgc.es>
2025-01-16 20:43:09 +0000

    Terraform now working.

--- main.tf
+++ main.tf
@@ -36,14 +36,13 @@ resource "aws_instance" "scraper_instance" {
 
   user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              amazon-linux-extras enable python3.8
-              yum install -y python3.8 git
-              pip3 install --upgrade pip
-              git clone https://github.com/CreamsCode/collector.git /home/ec2-user/scraper
+              sudo yum update -y
+              sudo yum install -y python git pip
+              sudo pip3 install --upgrade pip
+              sudo git clone https://github.com/CreamsCode/collector.git /home/ec2-user/scraper
               cd /home/ec2-user/scraper
-              pip3 install -r requirements.txt
-              python3 main.py --queue_url ${aws_sqs_queue.scraper_queue.url}
+              sudo pip3 install -r requirements.txt
+              sudo python3 main.py --queue_url ${aws_sqs_queue.scraper_queue.url}
               EOF
 
   tags = {
@@ -56,6 +55,6 @@ output "sqs_queue_url" {
   value = aws_sqs_queue.scraper_queue.url
 }
 
-output "ec2_public_ip" {
+output "collector_public_ip" {
   value = aws_instance.scraper_instance.public_ip
 }
