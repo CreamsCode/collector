fe42a296f6f2b2cc33808724147e21849353f0a3
Carlos <carlos.mendoza103@alu.ulpgc.es>
2025-01-18 01:49:16 +0000

    Testing tf

--- main.tf
+++ main.tf
@@ -1,20 +1,18 @@
 provider "aws" {
-  region = "us-east-1" # Cambia la región si es necesario
+  region = "us-east-1"
 }
 
-# Crear una VPC
-resource "aws_vpc" "scraper_vpc" {
+resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "Scraper-VPC"
+    Name = "Main-VPC"
   }
 }
 
-# Crear una subred
 resource "aws_subnet" "scraper_subnet" {
-  vpc_id                  = aws_vpc.scraper_vpc.id
+  vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
@@ -23,17 +21,15 @@ resource "aws_subnet" "scraper_subnet" {
   }
 }
 
-# Crear un gateway de Internet
 resource "aws_internet_gateway" "scraper_igw" {
-  vpc_id = aws_vpc.scraper_vpc.id
+  vpc_id = aws_vpc.main_vpc.id
   tags = {
     Name = "Scraper-IGW"
   }
 }
 
-# Crear una tabla de rutas para la VPC
 resource "aws_route_table" "scraper_route_table" {
-  vpc_id = aws_vpc.scraper_vpc.id
+  vpc_id = aws_vpc.main_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -45,16 +41,14 @@ resource "aws_route_table" "scraper_route_table" {
   }
 }
 
-# Asociar la tabla de rutas con la subred
 resource "aws_route_table_association" "scraper_route_table_assoc" {
   subnet_id      = aws_subnet.scraper_subnet.id
   route_table_id = aws_route_table.scraper_route_table.id
 }
 
-# Crear un grupo de seguridad para la instancia EC2
 resource "aws_security_group" "ec2_sg" {
   name_prefix = "scraper-sg"
-  vpc_id      = aws_vpc.scraper_vpc.id
+  vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
     from_port   = 22
@@ -104,7 +98,6 @@ resource "aws_instance" "scraper_instance" {
   }
 }
 
-# Salidas para información importante
 output "sqs_queue_url" {
   value = aws_sqs_queue.scraper_queue.url
 }
@@ -112,6 +105,16 @@ output "sqs_queue_url" {
 output "collector_public_ip" {
   value = aws_instance.scraper_instance.public_ip
 }
+
+resource "aws_ssm_parameter" "vpc_id" {
+  name  = "/shared/vpc/id"
+  type  = "String"
+  value = aws_vpc.main_vpc.id
+  tags = {
+    Name = "shared-vpc-id"
+  }
+}
+
 
 resource "aws_ssm_parameter" "sqs_queue_url" {
   name  = "sqs_queue_url"
